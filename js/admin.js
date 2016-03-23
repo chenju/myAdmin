b7266865b998ce1d6758b29d0dd828b0242ce683
xiao an <darkwingcj@gmail.com>
2016-03-23 09:49:30 +0800

    20160322

--- js/admin.js
+++ js/admin.js
@@ -1,23 +1,57 @@
-require('./api');
+//require('./api');
+require('./http')
 
-var myApp = angular.module('myApp', ['ng-admin']);
+var myApp = angular.module('myApp', ['ng-admin','http-auth-interceptor']);
 
 // custom API flavor
 
 // custom controllers
-myApp.controller('username', ['$scope', '$window', function($scope, $window) { // used in header.html
+myApp.controller('username', ['$scope', '$window', '$rootScope',function($scope, $window,$rootScope) { // used in header.html
     $scope.username =  $window.localStorage.getItem('posters_galore_login');
+    $rootScope.$on('event:auth-loginRequired',function(){
+
+        console.log('fuck')
+        alert('请注册')
+
+    })
 }])
 
 // custom states (pages)
 //myApp.config(['$stateProvider', require('./segments/segmentsState')]);
 
+
+myApp.config(['RestangularProvider', function(RestangularProvider) {
+    RestangularProvider.addElementTransformer('users', function(element) {
+        
+        console.log(element)
+
+        return element;
+    });
+}]);
+
+
+
+
 myApp.config(['NgAdminConfigurationProvider', function (nga) {
     // create the admin application
     var admin = nga.application('My First Admin')
-        .baseApiUrl('/');
+        .baseApiUrl('http://lumen.app/');
 
     // add entities
+    var posts = nga.entity('posts').identifier(nga.field('post_id'));;
+    admin.addEntity(posts);
+
+    posts.listView().fields([
+        nga.field('title'),
+        nga.field('post_id')
+    ])
+
+    var users = nga.entity('user');
+    admin.addEntity(users);
+
+    users.listView().fields([
+        nga.field('name')
+    ])
     
     nga.configure(admin);
 }]);
